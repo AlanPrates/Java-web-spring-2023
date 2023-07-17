1298f688d078a00f9fe47497821ac06310f8a02f
AlanPrates <61175274+AlanPrates@users.noreply.github.com>
2023-07-17 03:52:47 -0300

    Project
    Project completed successfully

---
+++ src/main/java/com/devsuperior/userdept/Departament.java
@@ -0,0 +1,28 @@
+package com.devsuperior.userdept;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_department")
+public class Departament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/devsuperior/userdept/UserRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.userdept;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+}
